2226fe827ecd3363fd3ec6b3e1710c943c8a633e
_BEK <62075760+BEK23@users.noreply.github.com>
2023-12-28 20:39:06 +0500

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import * as path from "node:path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/app"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
   plugins: [react()],
-})
+});
