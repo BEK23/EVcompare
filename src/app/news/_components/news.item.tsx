8802c8acb651e60af3de578a594c3e5a3cb6da90
_BEK <62075760+BEK23@users.noreply.github.com>
2023-12-29 15:08:05 +0500

    add news list

---
+++ src/app/news/_components/news.item.tsx
@@ -0,0 +1,20 @@
+import { INews } from "@/types/news.interface";
+
+export const NewsItem = (news: INews) => {
+  return (
+    <div className="relative h-[490px] overflow-hidden rounded-[1.25rem] border bg-background transition duration-500 hover:scale-105">
+      <div
+        style={{ backgroundImage: `url(${news.images[0]})` }}
+        className="h-[300px] w-full bg-cover bg-center bg-no-repeat shadow-[inset_0px_-30px_18px_-10px_var(--background)]"
+      />
+      <div className="absolute bottom-0 w-full space-y-4 px-5 pb-5">
+        <h3 className="text-center text-2xl font-bold">{news.title}</h3>
+        <p className="line-clamp-4">{news.description}</p>
+        <div className="flex justify-between font-bold">
+          <div>{news.brand}</div>
+          <div>${news.price}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/news/_components/news.tsx
+++ src/app/news/_components/news.tsx
@@ -1,11 +1,18 @@
 import { useQuery } from "react-query";
 
 import { getNews } from "@/api/news.api";
+import { NewsItem } from "@/app/news/_components/news.item";
 
 export const News = () => {
   const newsQuery = useQuery(["news", { limit: 8, skip: 0, q: "" }], getNews, {
     select: (response) => response.data,
   });
 
-  return <pre>{JSON.stringify(newsQuery.data, null, 2)}</pre>;
+  return (
+    <div className="grid grid-cols-1 gap-x-10 gap-y-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+      {newsQuery.data?.products.map((item) => (
+        <NewsItem key={item.id} {...item} />
+      ))}
+    </div>
+  );
 };
